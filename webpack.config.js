7c815384a03d6620ba0995a7644efad1ffe2ade8
Hassaanjbaig-code <80938128+Hassaanjbaig-code@users.noreply.github.com>
2022-12-07 00:20:50 +0500

    Add webpack

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    plugins: [
+      new HtmlWebpackPlugin({
+        hash: true,
+        title: 'Webpack Example App',
+        header: 'Webpack Example Title',
+        metaDesc: 'Webpack Example Description',
+        template: './src/index.html',
+        filename: 'index.html',
+        inject: 'body'
+      })
+    ],
+    mode: 'development',
+    output: {
+        clean: true
+      },
+      devServer: {
+        static: {
+          directory: path.join(__dirname, 'public'),
+        },
+        compress: true,
+        port: 9000,
+      },
+  };
